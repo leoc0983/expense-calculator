fff03211fccd3105400883c27db5be2e532a38f9
leochuang <lchuang3@uci.edu>
2025-09-26 10:23:22 -0700

    Added comments and data processing

--- data.py
+++ data.py
@@ -1,6 +1,9 @@
+#runs the program
 def main() -> None:
     data = get_input()
+    process_input(data)
 
+#asks the user for input and formats it as a list
 def get_input() -> list[tuple(str, float)]:
     expenses = []
     name = input('Enter the name of the expense (or x to quit): ')
@@ -10,6 +13,10 @@ def get_input() -> list[tuple(str, float)]:
         name = input('Enter the name of the expense (or x to quit): ')
         cost = input('Enter the cost of the expense: ')
     return expenses
+
+#performs calculations with the obtained data
+def process_input(data: list[tuple(str, float)]) -> None:
+    pass
 
 if __name__ == '__main__':
     main()
