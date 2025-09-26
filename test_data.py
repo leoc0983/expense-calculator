5c7415cf3899b08ea711ecc6f844c41ee27ad16b
leochuang <lchuang3@uci.edu>
2025-09-26 14:04:38 -0700

    Increased number of test cases for get_total_cost

--- test_data.py
+++ test_data.py
@@ -1,11 +1,18 @@
 import unittest
-import data
+from data import get_total_cost
+from data import get_report
 
 class TestData(unittest.TestCase):
-    def test_get_input(self):
-        pass
+    def test_get_total_cost(self):
+        data_set = [(1, 1), (2, 2), (3, 3), (4, 4), (5, 5)]
+        total_cost = get_total_cost(data_set)
+        self.assertEqual(total_cost, 15)
 
-    def test_process_data(self):
+        data_set = []
+        total_cost = get_total_cost(data_set)
+        self.assertEqual(total_cost, 0)
+
+    def test_get_report(self):
         pass
 
 if __name__ == '__main__':
