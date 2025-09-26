f940753470413a8afc25dbb29e2b25485a8240ed
leochuang <lchuang3@uci.edu>
2025-09-26 10:16:48 -0700

    Created the unittest module for data.py

--- data.py
+++ data.py
@@ -1,1 +1,15 @@
-//test
+def main() -> None:
+    data = get_input()
+
+def get_input() -> list[tuple(str, float)]:
+    expenses = []
+    name = input('Enter the name of the expense (or x to quit): ')
+    cost = input('Enter the cost of the expense: ')
+    while(name != 'x'):
+        expenses.append(tuple(name, cost))
+        name = input('Enter the name of the expense (or x to quit): ')
+        cost = input('Enter the cost of the expense: ')
+    return expenses
+
+if __name__ == '__main__':
+    main()
---
+++ test_data.py
@@ -0,0 +1,9 @@
+import unittest
+import data
+
+class TestData(unittest.TestCase):
+    def test_get_input(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
