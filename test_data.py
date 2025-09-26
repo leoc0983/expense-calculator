06b71bd4b8db730c8e5c6b44c97c8c020cb7b6fe
leochuang <lchuang3@uci.edu>
2025-09-26 14:12:15 -0700

    Increased number of test cases for get_report

--- test_data.py
+++ test_data.py
@@ -13,7 +13,12 @@ class TestData(unittest.TestCase):
         self.assertEqual(total_cost, 0)
 
     def test_get_report(self):
-        pass
+        data_set = [(1.00, 'rent'), (2.01, 'groceries'), (3.02, 'gas')]
+        self.assertEqual(get_report(data_set),
+        'Total cost of all items: $6.03\n' +
+        'Expense: rent Price: $1.0 Percent of total cost: 16.58%\n' +
+        'Expense: groceries Price: $2.01 Percent of total cost: 33.33%\n' +
+        'Expense: gas Price: $3.02 Percent of total cost: 50.08%\n')
 
 if __name__ == '__main__':
     unittest.main()
