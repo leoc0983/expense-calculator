904ae28d0abf1778b8623a533bc521d4ed1bb25e
leochuang <lchuang3@uci.edu>
2025-09-27 15:20:33 -0700

    Created client.py

---
+++ client.py
@@ -0,0 +1,27 @@
+import socket
+
+def start_client():
+    with socket.socket() as expense_socket:
+
+        connect_address = ('127.0.0.1', 8000)
+        expense_socket.connect(connect_address)
+
+        input_stream = expense_socket.makefile('r')
+        output_stream = expense_socket.makefile('w')
+
+        output_stream.write(get_input())
+        output_stream.flush()
+
+        print(input_stream.readline())
+
+def get_input() -> str:
+    expenses = ''
+    name = input('Enter the name of the expense (or q to quit): ')
+    while name != 'q':
+        cost = input('Enter the cost of ' + name + ': ')
+        expenses += ' ' + cost + '\n'
+        name = input('Enter the name of the expense (or q to quit): ')
+    return expenses
+
+if __name__ == '__main__':
+    start_client()
