d0cc36d6c6255580804e2575edf5f2ed3a999f77
leochuang <lchuang3@uci.edu>
2025-09-27 17:10:43 -0700

    Added storage of data with SQL

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import socket
+import sqlite3
 
 def start_server(host_address, host_port):
     with socket.socket() as srv:
@@ -20,6 +21,18 @@ def start_server(host_address, host_port):
             connection.sendall(rec_msg)
 
             print('Client disconnected')
+
+def create_db():
+    connection = sqlite3.connect('expenses.db')
+    cursor = connection.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS expenses (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        cost INTEGER NOT NULL
+    ''')
+    connection.commit()
+    connection.close()
 
 if __name__ == '__main__':
     start_server('localhost', 8000)
